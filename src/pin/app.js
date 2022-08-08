db2acf6ca1136d22ff29ae3cda7a45b7470c1a7c
BoPin1219 <106055273+BoPin1219@users.noreply.github.com>
2022-08-08 11:47:41 +0800

    0808

--- src/pin/app.js
+++ src/pin/app.js
@@ -3,8 +3,8 @@ import React from 'react'
 
 import CompanyLogin from './company_Login/company_login'
 import LoginRegister from './company_login_register/login_register'
-// import CompanyHome from './company_Home/Company_Home'
-// import CompanyActivity from './company_Activity/company_activity'
+import CompanyHome from './Company_Home/Company_Home'
+import CompanyActivity from './Company_Activity/Company_Activity'
 import ManageList from '../page/ManageList'
 
 function Company() {
@@ -13,8 +13,8 @@ function Company() {
             <Routes>
                 <Route path="/login" element={<CompanyLogin />} />
                 <Route path="/register" element={<LoginRegister />} />
-                {/* <Route path="/companyhome" element={<CompanyHome />} /> */}
-                {/* <Route path="/activity" element={<CompanyActivity />} /> */}
+                <Route path="/companyhome" element={<CompanyHome />} />
+                <Route path="/activity" element={<CompanyActivity />} />
                 <Route path="/product" element={<ManageList />} />
             </Routes>
         </>
